fbe077bee411b4900e3c2a18a0dfeda75cb240c0
jfmenard <jfmenard@hotmail.fr>
2022-11-08 17:17:54 +0100

    modif-backend-update

--- routes/trips.js
+++ routes/trips.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var Trip = require('../models/trips')
 
-router.get('/', function(req, res) {
+router.post('/', function(req, res) {
  res.json({result: req.body.departure})
 });
 
