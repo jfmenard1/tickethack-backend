46d0f71d64642940c0ed2c2bdddbaeea1e03610f
Leopold van der Elst <leopoldvde@hotmail.com>
2022-11-09 14:26:01 +0100

    commit

--- routes/trips.js
+++ routes/trips.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var moment = require('moment');
 var Trip = require('../models/trips')
 
 router.post('/', function(req, res) {
- res.json({result: req.body.departure})
+const departure = req.body.departure
+const arrival = req.body.arrival
+const date = new Date(req.body.date)
+        Trip.find({departure: departure, arrival: arrival, date: { $gte: moment(date).startOf('day'), $lte: moment(date).endOf('day')}})
+        .then(data => {
+        res.json({result: data})
+        })
+  
 });
 
-module.exports = router;
+module.exports = router; 
+
+//date: { $gte: moment(date).startOf('day'), $lte: moment(date).endOf('day') } 
